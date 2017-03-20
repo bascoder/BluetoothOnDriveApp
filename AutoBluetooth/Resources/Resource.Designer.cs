78fd7a1f14b4cc882bb4ca69d580bf1719106237
Bas van Marwijk <basvmar@hotmail.com>
2017-03-20 18:05:35 +0100

    Setup basic UI

--- AutoBluetooth/Resources/Resource.Designer.cs
+++ AutoBluetooth/Resources/Resource.Designer.cs
@@ -57,6 +57,34 @@ namespace AutoBluetooth
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int btnStartService = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int chSensorSupported = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int tvDetectedActivityPlaceholder = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int tvLastDetectedActivity = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int tvSensorSupported = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -76,11 +104,14 @@ namespace AutoBluetooth
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int ApplicationName = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int last_detected_activity = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int sensor_supported = 2130968577;
 			
 			static String()
 			{
